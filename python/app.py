e0d0d66762b55efbd19c809fde48fdb951be7434
Sumit Mukherjee <69989028+summu1985@users.noreply.github.com>
2025-04-11 09:27:42 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -12,7 +12,7 @@ def hello():
 @app.route('/metrics',methods=['GET'])
 def metrics():
     # return f'custom_metric_1 {random.randint(0, 100)}\n'
-    return f'custom_metric_1 40\n'
+    return f'custom_metric_1 5\n'
 
 if __name__ == "__main__":
   app.run(debug=False,host="0.0.0.0", port=8080)
